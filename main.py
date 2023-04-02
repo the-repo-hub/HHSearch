8663fe7ae7620ea0e617e660ead238b12d3e46d8
localhost <localhost@localhost>
2023-04-02 11:39:58 +0300

    upgraded

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
-from selenium.webdriver import Firefox
+from selenium import webdriver
+from selenium.webdriver.common.by import By
 import asyncio
 import ast
-from selenium.webdriver.common.by import By
 import bs4
 import aiohttp
+import threading
+
+
+vacancies_url = 'https://spb.hh.ru/search/vacancy?resume=c6f3d876ff0ba2033d0039ed1f6a6f4e386247&search_field=company_name&search_field=description&search_field=name&forceFiltersSaving=true&enable_snippets=false&salary=80000&from=resumelist'
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:110.0) Gecko/20100101 Firefox/110.0'}
+
+try:
+    with open('cookies.txt') as file:
+        cookies = ast.literal_eval(file.read())
+        aiocookies = {}
+        for c in cookies:
+            aiocookies[c['name']] = c['value']
+except FileNotFoundError:
+    exit("You need to insert cookies (as selenium's .get_cookies() in cookies.txt file)!")
+except SyntaxError:
+    exit('Invalid cookies!')
+
+try:
+    with open('letter.txt') as file:
+        letter = file.read()
+except FileNotFoundError:
+    exit("You need write a letter and put it in letter.txt!")
 
 
 class Vacancy:
     all = []
-    ALLOW_WORDS = ['Middle', 'Python', 'Backend']
-    DENY_WORDS = ['Senior', 'Java', 'Lead', 'JS', 'Нейрон', 'Goolang', 'PHP', 'C++', 'Ведущий', 'C#']
+    ALLOW_WORDS = ['Python']
+    DENY_WORDS = ['Senior', 'Lead', 'Нейрон', 'Лид', 'Детей', 'Преподаватель', 'Наставник']
 
     ALLOW_WORDS += list(map(lambda s: s.lower(), ALLOW_WORDS))
     DENY_WORDS += list(map(lambda s: s.lower(), DENY_WORDS))
@@ -34,9 +56,9 @@ class Vacancy:
                 for deny in self.DENY_WORDS:
                     if deny in self.name:
                         flag = False
-                if flag:
-                    self.all.append(self)
-                    break
+            if flag:
+                self.all.append(self)
+                break
 
     def __str__(self):
         return self.name
@@ -44,19 +66,12 @@ class Vacancy:
 
 async def generate_queries():
     gather = []
-    vacancies_url = 'https://spb.hh.ru/search/vacancy?resume=c6f3d876ff0ba2033d0039ed1f6a6f4e386247&search_field=company_name&search_field=description&search_field=name&forceFiltersSaving=true&enable_snippets=false&salary=80000&from=resumelist'
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:110.0) Gecko/20100101 Firefox/110.0'}
 
-    with open('cookies.txt') as file:
-        cookies = {}
-        for c in ast.literal_eval(file.read()):
-            cookies[c['name']] = c['value']
-
-    async with aiohttp.ClientSession(headers=headers, cookies=cookies) as session:
+    async with aiohttp.ClientSession(headers=headers, cookies=aiocookies) as session:
         first_page = await session.get(vacancies_url)
         num = int(bs4.BeautifulSoup(await first_page.text(), 'lxml').find(attrs={'data-qa': "pager-block"}).find_all('span', attrs={
             'class': ['pager-item-not-in-short-range']})[-1].find('a', attrs={'data-qa': "pager-page"}).text)
-
+        print(f'Pages: {num}')
         for i in range(1, num + 1):
             gather.append(session.get(vacancies_url + f'&page={i}'))
         gather = await asyncio.gather(*gather)
@@ -65,23 +80,38 @@ async def generate_queries():
             vacancies = bs4.BeautifulSoup(await g.text(), 'lxml').find_all(class_="serp-item")
             for vac in vacancies:
                 Vacancy(vac)
-        d = 3
+    print(f'Got {len(Vacancy.all)} vacancies')
 
 
-def firefox_driver():
-    driver = Firefox()
+def firefox_driver(obj_for_driver):
+    driver = webdriver.Firefox()
     driver.get('https://spb.hh.ru/account/login?backurl=%2F&hhtmFrom=main')
-    vacancies_url = 'https://spb.hh.ru/search/vacancy?resume=c6f3d876ff0ba2033d0039ed1f6a6f4e386247&search_field=company_name&search_field=description&search_field=name&forceFiltersSaving=true&enable_snippets=false&salary=80000&from=resumelist'
+    driver.delete_all_cookies()
+    for c in cookies:
+        driver.add_cookie(c)
+    driver.get('https://spb.hh.ru')
+    obj_for_driver['driver'] = driver
 
-    with open('cookies.txt') as cookies:
-        for c in ast.literal_eval(cookies.read()):
-            driver.add_cookie(c)
 
-    driver.get(vacancies_url)
+def send_letter(driver: webdriver.Firefox, vac: Vacancy):
+    driver.get(vac.link)
+    experience = driver.find_element(By.XPATH, '/html/body/div[5]/div/div[3]/div[1]/div/div/div/div/div[1]/div[1]/div[1]/div/p[1]/span').text
+    if experience == '1–3':
+        button = driver.find_element(By.XPATH, '/html/body/div[5]/div/div[3]/div[1]/div/div/div/div/div[1]/div[1]/div[1]/div/div[3]/div[2]/div/div[1]/a')
+        button.click()
+
+
+def main():
+    obj_for_driver = {}
+    t1 = threading.Thread(target=firefox_driver, args=(obj_for_driver,))
+    t1.start()
+    # asyncio.run(generate_queries())
+    t1.join()
+    obj_for_driver['driver'].get('https://spb.hh.ru/vacancy/72216524?from=vacancy_search_list')
+    for vac in Vacancy.all:
+        send_letter(obj_for_driver['driver'], vac)
 
 
 if __name__ == '__main__':
-    #main()
-    asyncio.run(generate_queries())
-    print(Vacancy.all)
+    main()
 
